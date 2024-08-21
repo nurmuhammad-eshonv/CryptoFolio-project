073e82ab064d09d109f2a04f277240e192e99312
nurmuhammad <nurmuhammadeshonov97@gmail.com>
2024-08-21 11:42:29 +0500

    added functoin IsInwaychList

--- src/components/MarketCup.jsx
+++ src/components/MarketCup.jsx
@@ -1,5 +1,4 @@
 
-
 import React, { useState, useEffect } from "react";
 import { MdRemoveRedEye } from "react-icons/md";
 
@@ -36,30 +35,25 @@ const MarketCup = () => {
     }
   };
 
-  const handleBringData = (item) => {  
-    setModalData(prevData => {
-        if (prevData.some(data => data.id === item.id)) {
-            return prevData; 
-        }
-        return [...prevData, item]; 
+  const handleBringData = (item) => {
+    setModalData((prevData) => {
+      if (prevData.some((data) => data.id === item.id)) {
+        return prevData;
+      }
+      return [...prevData, item];
     });
-    
+
     setIsModalOpen(true);
-};
+  };
+
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
 
-  function handleDeleteModal (item){
-        const newDate = modalData.filter((data) => {
-          return data.id!== item.id;
-        })
-        setModalData(newDate);
-        //  const newDeletedData = [...modalData]
-    //   newDeletedData.pop()
-      setModalData(newDeletedData)
-  }
- 
+  const handleDeleteModal = (item) => {
+    const newDeletedData = modalData.filter((data) => data.id !== item.id);
+    setModalData(newDeletedData);
+  };
 
   return (
     <div className="bg-transparent p-4 rounded-lg containerr">
@@ -88,36 +82,41 @@ const MarketCup = () => {
             .filter((item) => {
               return item.symbol.toLowerCase().includes(search.toLowerCase());
             })
-            .map((item, index) => (
-              <tr key={index} className="text-white border-b border-gray-800">
-                <td className="p-2 flex items-center">
-                  <img
-                    className="w-[50px] h-[50px]"
-                    src={item.image}
-                    alt={item.name}
-                  />
-                  <p className="ml-2 text-2xl m-6">{item.symbol.toUpperCase()}</p>
-                  <p className="absolute mt-[40px] ml-[60px] text-gray-400 text-[12px]">
-                    {item.name}
-                  </p>
-                </td>
-                <td className="pl-[290px]">₹{item.current_price}</td>
-                <td
-                  className={`pl-20 ${
-                    item.price_change_percentage_24h > 0
-                      ? "text-green-500"
-                      : "text-red-500"
-                  }`}
-                >
-                  <MdRemoveRedEye
-                    onClick={() => handleBringData(item)}
-                    className="cursor-pointer absolute -ml-5 mt-[2px]"
-                  />
-                  {item.price_change_percentage_24h}%
-                </td>
-                <td className="pl-20">₹{item.market_cap}</td>
-              </tr>
-            ))}
+            .map((item, index) => {
+              const isInWatchList = modalData.some((data) => data.id === item.id);
+              return (
+                <tr key={index} className="text-white border-b border-gray-800">
+                  <td className="p-2 flex items-center">
+                    <img
+                      className="w-[50px] h-[50px]"
+                      src={item.image}
+                      alt={item.name}
+                    />
+                    <p className="ml-2 text-2xl m-6">{item.symbol.toUpperCase()}</p>
+                    <p className="absolute mt-[40px] ml-[60px] text-gray-400 text-[12px]">
+                      {item.name}
+                    </p>
+                  </td>
+                  <td className="pl-[290px]">₹{item.current_price}</td>
+                  <td
+                    className={`pl-20 ${
+                      item.price_change_percentage_24h > 0
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }`}
+                  >
+                    <MdRemoveRedEye
+                      onClick={() => handleBringData(item)}
+                      className={`cursor-pointer absolute -ml-5 mt-[2px] ${
+                        isInWatchList ? "text-green-500" : "text-white"
+                      }`}
+                    />
+                    {item.price_change_percentage_24h}%
+                  </td>
+                  <td className="pl-20">₹{item.market_cap}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
       <div className="flex justify-center mt-4">
@@ -139,10 +138,13 @@ const MarketCup = () => {
       {isModalOpen && modalData.length > 0 && (
         <div className="absolute top-0 left-[1138px] ">
           <div className="p-8 rounded-lg w-[550px] bg-gray-500 text-gray-800 flex flex-wrap gap-8 justify-center">
-          <h1 className="text-[40px] ml-[66px] text-white">WatchList</h1>
+            <h1 className="text-[40px] ml-[66px] text-white">WatchList</h1>
 
             {modalData.map((item, index) => (
-              <div key={index} className="w-[198px] h-[230px] flex flex-col items-center rounded-[25px] justify-center bg-black mb-4">
+              <div
+                key={index}
+                className="w-[198px] h-[230px] flex flex-col items-center rounded-[25px] justify-center bg-black mb-4"
+              >
                 <img className="w-[118px]" src={item.image} alt="" />
                 <h1
                   className={`pt-3 ${
@@ -154,9 +156,13 @@ const MarketCup = () => {
                   {item.price_change_percentage_24h}%
                 </h1>
 
-                <button onClick={() => handleDeleteModal(item)} className="bg-red-600 mt-1 px-2">remove</button>
+                <button
+                  onClick={() => handleDeleteModal(item)}
+                  className="bg-red-600 mt-1 px-2"
+                >
+                  remove
+                </button>
               </div>
-
             ))}
             <button
               onClick={handleCloseModal}
@@ -166,7 +172,6 @@ const MarketCup = () => {
             </button>
           </div>
         </div>
-      //  
       )}
     </div>
   );
